8706110743af4987c128b25b455785d1ade3a4f8
zbrittenSAIC <zachary.t.britten@saic.com>
2023-11-03 14:32:00 -0500

    updated parameter names

--- ejscreen_dataset.py
+++ ejscreen_dataset.py
@@ -16,36 +16,43 @@ if __name__ == '__main__':
     
     #set `option` to 1 to generate national percentiles.
     #set `option` to 2 to generate state percentiles
-    option = 1
+    level = 1
 
     #path to input csv dataset
-    input_ejscreen_csv = "data/EJSCREEN_2023_BG_with_AS_CNMI_GU_VI.csv"
+    input_csv_path = "data/EJSCREEN_2023_BG_with_AS_CNMI_GU_VI.csv"
 
     #path to output csv dataset
-    output_ejscreen_csv = "data/EJSCREEN_Output.csv"
+    output_csv_path = "data/EJSCREEN_Output.csv"
 
     #path to output lookuptable excel file
-    output_lookuptable_xlsx = "data/lookup.xlsx"
+    lookuptable_xlsx_path = "data/lookup.xlsx"
 
     #whether or not you wish to join the output to geometry and export to ESRI Feature Class
     output_to_featureclass = True
 
     #path to geometry which the output table will be joined
-    geometry_source_featureclass = "data/BlockGroups.gdb/BG"
+    geometry_featureclass_path = "data/BlockGroups.gdb/BG"
 
     #path to output ESRI Feature Class
-    output_featureclass_name = "data/BlockGroups.gdb/EJSCREEN_Output"
+    output_featureclass_path = "data/BlockGroups.gdb/EJSCREEN_Output"
 
     #path to ESRI schema csv file 
-    schema_csv = "data/ejscreen_schema.csv"
+    schema_csv_path = "data/ejscreen_schema.csv"
 
 #*************************************************************************************************************************************    
-    if option != 1 and option != 2:
-        sys.exit("`option` must have a value of either 1 or 2")
+    if level != 1 and level != 2:
+        sys.exit("`level` must have a value of either 1 or 2")
     
     if output_to_featureclass == True:
-        if not geometry_source_featureclass or not output_featureclass_name or not schema_csv:
+        if not geometry_featureclass_path or not output_featureclass_path or not schema_csv_pathv:
             sys.exit("If outputing to featureclass, all parameters must have a value")
     
-    main(option, input_ejscreen_csv, output_ejscreen_csv, output_lookuptable_xlsx, output_to_featureclass, geometry_source_featureclass, output_featureclass_name, schema_csv)
+    main(level, 
+    input_csv_path, 
+    output_csv_path, 
+    lookuptable_xlsx_path, 
+    output_to_featureclass, 
+    geometry_featureclass_path, 
+    output_featureclass_path, 
+    schema_csv_path)
 
